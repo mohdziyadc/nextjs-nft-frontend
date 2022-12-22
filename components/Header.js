0859e690f47f6024e9c0f3c4704e099f42711d43
Mohd Ziyad <mziyadc@gmail.com>
2022-12-23 00:38:14 +0530

    Moralis Server

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,22 @@
 //yarn add web3uikit moralis react-moralis
 import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+
 export default function Header() {
-    return <ConnectButton />;
+    return (
+        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+            <h1 className="py-4 px-4 font-bold text-3xl flex-1">
+                NFT Marketplace
+            </h1>
+            <div className="flex flex-row  items-center">
+                <Link href="/" className="mr-4 p-6">
+                    Home
+                </Link>
+                <Link href="/sell-nft" className="mr-4 p-6">
+                    Sell NFT
+                </Link>
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </nav>
+    );
 }
